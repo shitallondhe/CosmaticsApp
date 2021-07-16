1ad0732f43bc98a4cb64b49c86d8633a5551aee9
shitallondhe <shital.londhe21@gmail.com>
2021-07-16 19:27:24 +0530

    Fifth Commit Final Code

--- app/src/main/java/com/example/cosmeticshop/di/module/ApplicationModule.kt
+++ app/src/main/java/com/example/cosmeticshop/di/module/ApplicationModule.kt
@@ -13,6 +13,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 
@@ -29,6 +30,10 @@ class ApplicationModule {
         val loggingInterceptor = HttpLoggingInterceptor()
         loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
         OkHttpClient.Builder()
+            .callTimeout(2, TimeUnit.MINUTES)
+            .connectTimeout(20, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
             .addInterceptor(loggingInterceptor)
             .build()
     } else OkHttpClient
